1a9b18412d5e1240979748b5d232f433c38b4e2a
MinxSu <minxsu.work@gmail.com>
2025-09-09 00:26:57 +0800

    add minor_arcana

--- lambda_function.py
+++ lambda_function.py
@@ -1,50 +1,35 @@
 import random
+from cards import major_arcana, minor_arcana 
 
 from bedrock_service import BedrockService 
 
 def lambda_handler(event, context):
-    print("測試程式更新。")
     cards = []
     while len(cards) < 3:
-        card = draw_card()
+        deck = major_arcana + minor_arcana
+        shuffled = shuffle(deck)
+        card = draw_card(shuffled)
         if not card in cards:
             cards.append((card, position()))
-    bedrock = BedrockService()
-    response = bedrock.tarot_reading()
+        return cards
+    #　bedrock = BedrockService()
+    # response = bedrock.tarot_reading()
 
-    return {
-        'statusCode': 200,
-        'body': response
-    }
+    # return {
+    #     'statusCode': 200,
+    #     'body': response
+    # }
 
-# 22張大阿爾克納牌
-major_arcana = [
-  "Ⅰ 愚者 — The Fool",
-  "Ⅱ 魔術師 — The Magician",
-  "Ⅲ 女祭司 — The High Priestess",
-  "Ⅳ 皇后 — The Empress",
-  "Ⅴ 皇帝 — The Emperor",
-  "Ⅵ 教宗 — The Hierophant",
-  "Ⅶ 戀人 — The Lovers",
-  "Ⅷ 戰車 — The Chariot",
-  "Ⅸ 力量 — Strength",
-  "Ⅹ 隱者 — The Hermit",
-  "Ⅺ 命運之輪 — Wheel of Fortune",
-  "Ⅻ 正義 — Justice",
-  "ⅩⅢ 吊人 — The Hanged Man",
-  "ⅩⅣ 死神 — Death",
-  "ⅩⅤ 節制 — Temperance",
-  "ⅩⅥ 惡魔 — The Devil",
-  "ⅩⅦ 高塔 — The Tower",
-  "ⅩⅧ 星星 — The Star",
-  "ⅩⅨ 月亮 — The Moon",
-  "ⅩⅩ 太陽 — The Sun",
-  "ⅩⅪ 審判 — Judgement",
-  "ⅩⅫ 世界 — The World"
-]
+# 抽牌
+def draw_card(shuffled_card):
+    return random.choice(shuffled_card)
 
-def draw_card():
-    return random.choice(major_arcana)
-
+# 正位/逆位
 def position():
     return random.choice(['Upright', 'Reversed'])
+
+# 洗牌
+def shuffle(deck):
+    shuffled = deck[:]       
+    random.shuffle(shuffled)
+    return shuffled
